7fc76b79c57721f0e283dd55d53363fb51a0c573
dk10119 <tranduclong092@gmail.com>
2024-05-09 11:45:11 +0300

    Feat: add form field type for email and submit

--- src/Form/SubscriberFormType.php
+++ src/Form/SubscriberFormType.php
@@ -4,22 +4,21 @@ namespace App\Form;
 
 use App\Entity\Subscriber;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class SubscriberFormType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
+class SubscriberFormType extends AbstractType {
+    public function buildForm(FormBuilderInterface $builder, array $options): void {
         $builder
             ->add('firstName')
             ->add('lastName')
-            ->add('email')
-        ;
+            ->add('email', EmailType::class)
+            ->add('submit', SubmitType::class);
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
-    {
+    public function configureOptions(OptionsResolver $resolver): void {
         $resolver->setDefaults([
             'data_class' => Subscriber::class,
         ]);
