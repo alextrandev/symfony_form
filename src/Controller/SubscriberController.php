a2a222ea00139aae4f266894a410a3535a4ca4e1
dk10119 <tranduclong092@gmail.com>
2024-05-09 12:31:51 +0300

    Feat: handle form submit

--- src/Controller/SubscriberController.php
+++ src/Controller/SubscriberController.php
@@ -4,15 +4,25 @@ namespace App\Controller;
 
 use App\Entity\Subscriber;
 use App\Form\SubscriberFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SubscriberController extends AbstractController {
     #[Route('/subscriber', name: 'app_subscriber')]
-    public function show(): Response {
+    public function subscribe(Request $req, EntityManagerInterface $em): Response {
         $subscriber = new Subscriber(); //create a new entity instance
         $form = $this->createForm(SubscriberFormType::class, $subscriber); //create new form and connect fields with entity object
+
+        $form->handleRequest($req);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($subscriber);
+            $em->flush();
+            return new Response("Subscriber number {$subscriber->getID()} created");
+        }
 
         return $this->render('subscriber/index.html.twig', [
             'subscriber_form' => $form->createView()
