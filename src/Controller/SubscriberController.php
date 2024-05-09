9b0beae3aa980f664895ff1fc1df93016b5c44e9
dk10119 <tranduclong092@gmail.com>
2024-05-09 11:57:28 +0300

    Feat: create a simple form and pass view to template

--- src/Controller/SubscriberController.php
+++ src/Controller/SubscriberController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Subscriber;
+use App\Form\SubscriberFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class SubscriberController extends AbstractController
-{
+class SubscriberController extends AbstractController {
     #[Route('/subscriber', name: 'app_subscriber')]
-    public function index(): Response
-    {
+    public function show(): Response {
+        $subscriber = new Subscriber(); //create a new entity instance
+        $form = $this->createForm(SubscriberFormType::class, $subscriber); //create new form and connect fields with entity object
+
         return $this->render('subscriber/index.html.twig', [
-            'controller_name' => 'SubscriberController',
-        ]);
+            'subscriber_form' => $form->createView()
+        ]); //create form view and pass to template
     }
 }
